b76b5bf3452d9b89acd517d51e9e95c989a74cb0
Ali Samadi <alisamadi.096@gmail.com>
2025-03-26 15:27:37 -0500

    write functions (signup) and (authenticate) to compelte the AuthContext.jsx

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -9,10 +9,44 @@ export function AuthProvider({ children }) {
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  async function signup() {
+    try {
+      const response = await fetch(API, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "AliSamadi",
+          password: "Gholi27096",
+        }),
+      });
+      const result = await response.json();
+      return result.token;
+      setToken(result.token);
+      setLocation("TABLET");
+    } catch (e) {
+      console.error("oh no ;(");
+    }
+  }
 
   // TODO: authenticate
-
-  const value = { location };
+  async function authenticate(token) {
+    try {
+      const response = await fetch(API, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const result = await response.json();
+      return result;
+      setLocation("TUNNEL");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  const value = { location, token , signup , authenticate};
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
